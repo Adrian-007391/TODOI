add6378e8e7426bbeae14281018bd924dd09c368
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-07-12 15:45:14 -0500

    Correccion de las listas y implementacion del task container

---
+++ src/Componentes/taskcontainer/Taskcontainer.tsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+
+function Taskcontainer(){
+    return(
+       <div>
+           <h1>
+               task container
+           </h1>
+       </div>
+    )
+
+}
+export default Taskcontainer
